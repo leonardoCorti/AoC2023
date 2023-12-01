1ba363d48e11f70d38ebed4a4b24a2d9f90a4c15
Leonardo Corti <leonardo.filippo@ymail.com>
2023-12-01 20:38:58 +0100

    day 01 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::{fs::{self}, collections::HashMap};
+use std::{fs::{self}, collections::{HashMap, HashSet, hash_map}};
 
 fn main() {
     println!("Hello, world!");
     let input: String = fs::read_to_string("./inputs/1.txt").unwrap();
-    println!("part 1: {}", part_1(input));
+    println!("part 1: {}", part_1(input.clone()));
+    println!("part 2: {}", part_2(input.clone()));
 }
 
 pub fn part_1(input: String) -> String {
@@ -22,20 +23,36 @@ pub fn part_1(input: String) -> String {
 }
 
 fn part_2(input:String) -> String {
-    let mut numbers_from_words= HashMap::new();
 
-    numbers_from_words.insert("one", 1);
-    numbers_from_words.insert("two", 2);
-    numbers_from_words.insert("three", 3);
-    numbers_from_words.insert("four", 4);
-    numbers_from_words.insert("five", 5);
-    numbers_from_words.insert("six", 6);
-    numbers_from_words.insert("seven", 7);
-    numbers_from_words.insert("eight", 8);
-    numbers_from_words.insert("nine", 9);
-    numbers_from_words.insert("zero", 0);
+    let mut string_to_number: HashMap<&str, &str> = HashMap::new();
+    string_to_number.insert("one", "1");
+    string_to_number.insert("two", "2");
+    string_to_number.insert("three", "3");
+    string_to_number.insert("four", "4");
+    string_to_number.insert("five", "5");
+    string_to_number.insert("six", "6");
+    string_to_number.insert("seven", "7");
+    string_to_number.insert("eight", "8");
+    string_to_number.insert("nine", "9");
+    string_to_number.insert("zero", "0");
 
-    return "0".to_string();
+    let mut new_input = input.clone();
+
+    let mut specific_cases : HashMap<&str, &str> = HashMap::new();
+    specific_cases.insert("eightwo", "82");
+    specific_cases.insert("eighthree", "83");
+    specific_cases.insert("twone", "21");
+    specific_cases.insert("oneight", "18");
+    specific_cases.insert("nineight", "98");
+
+    for (string_form, number_form) in specific_cases {
+        new_input = new_input.replace(string_form, number_form);        
+    }
+
+    for (string_form, number_form) in string_to_number {
+        new_input = new_input.replace(string_form, number_form);        
+    }
+    return part_1(new_input)
 }
 
 #[cfg(test)]
