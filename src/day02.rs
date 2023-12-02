93bf4c9d11213cbfd4715743028c54272f7cb4a1
Leonardo Corti <leonardo.filippo@ymail.com>
2023-12-02 10:36:52 +0100

    Day 02 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -23,7 +23,6 @@ pub fn part_1(input: String) -> String {
     let mut sum_of_ids: i32 = 0;
 
     for (id, game) in games{
-        let (r,g,b) = (false, false, false);
         let matches: Vec<&str> = game.split(";").collect();
         let mut too_much = false;
 
@@ -62,7 +61,53 @@ pub fn part_1(input: String) -> String {
 
 pub fn part_2(input: String) -> String {
 
-    return "todo!".to_string();
+    let games: Vec<(i32, String)> = input.lines().map(|line|{
+        let parts: Vec<&str> = line.split(":").collect();
+        let id: i32 = parts[0].trim().split_once(" ").unwrap().1.trim().parse().unwrap();
+        (id, parts[1].to_string())
+    }).collect();
+
+    let result: i32 = games.iter().map(|(_, game)|{
+        
+        let matches: Vec<&str> = game.split(";").collect();
+        let (mut red_min, mut blue_min, mut green_min) = (-1, -1, -1);
+
+        for single_match in matches{
+            let cubes: Vec<&str> = single_match.split(",").collect();            
+            let (mut red, mut green, mut blue) = (0, 0 ,0);
+
+            for c in cubes {
+                if c.contains("blue"){
+                    blue = c.replace("blue", "").trim().parse().unwrap();
+                }
+                else if c.contains("red"){
+                    red = c.replace("red", "").trim().parse().unwrap();
+                }
+                else if c.contains("green"){
+                    green = c.replace("green", "").trim().parse().unwrap();
+                }
+                else {
+                    panic!("no blue red or green");
+                }
+            }
+
+            if red_min == -1 || red_min < red {
+                red_min = red;
+            }
+            if blue_min == -1 || blue_min < blue {
+                blue_min = blue;
+            }
+            if green_min == -1 || green_min < green {
+                green_min = green;
+            }
+            
+        }
+
+        return green_min*blue_min*red_min;
+
+    }).sum();
+
+    return result.to_string();
 }
 
 
@@ -82,7 +127,11 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        let input = "".to_string();
-        assert_eq!("281", part_2(input));
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".to_string();
+        assert_eq!("2286", part_2(input));
     }
 }
