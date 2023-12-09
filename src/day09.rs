3d369bc38228a7d331747fb7561a1b40beabedca
Leonardo Corti <leonardo.filippo@ymail.com>
2023-12-09 10:23:41 +0100

    Day 09 part 1

---
+++ src/day09.rs
@@ -0,0 +1,93 @@
+use std::fs;
+
+pub(crate) fn day_09() {
+    let input: String = fs::read_to_string("./inputs/9.txt").unwrap();
+    println!("day 09:");
+    println!("  part 1: {}", part_1(input.clone()));
+    println!("  part 2: {}", part_2(input.clone()));
+}
+
+pub fn part_1(input: String) -> String {
+
+    let result: isize = input.lines()
+        .map(|line| parse_line(line))
+        .map(|sequence| extrapolate_new_number(sequence))
+        .sum();
+
+    result.to_string()
+}
+
+fn parse_line(line: &str) -> Vec<isize> {
+    let result: Vec<isize> = line.split_ascii_whitespace()
+        .map(|n| n.parse().unwrap())
+        .collect();
+    result
+}
+
+pub(crate) fn part_2(input:String) -> String {
+    todo!();
+}
+
+fn extrapolate_new_number(initial_sequence: Vec<isize>) -> isize {
+
+    let mut all_sequences: Vec<Vec<isize>> = Vec::new();
+    all_sequences.push(initial_sequence);
+    
+    while !is_all_zero( all_sequences.last().unwrap() ) {
+        let mut new_sequence: Vec<isize> = Vec::new();
+        let last_sequence = all_sequences.last().unwrap();
+        for i in 0..(last_sequence.len()-1) {
+            new_sequence.insert(i, 
+                last_sequence.get(i+1).unwrap()-last_sequence.get(i).unwrap()
+            )
+        }
+        all_sequences.push(new_sequence);
+    }
+    all_sequences.pop(); //remove the only 0 sequence
+    
+    let mut new_number: isize = 0;
+
+    for seq in all_sequences.iter().rev(){
+        let last_number: &isize = seq.last().unwrap();
+        new_number = new_number + *last_number;
+    }
+    new_number
+}
+
+fn is_all_zero(vector: &Vec<isize>) -> bool {
+    for value in vector {
+        if *value!=0{
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = 
+"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
+    ; 
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!("114", part_1(TEST_INPUT.to_string()));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!("todo", part_2(TEST_INPUT.to_string()));
+    }
+
+    #[test]
+    fn test_extrapolate_number() {
+        assert_eq!(18, extrapolate_new_number(vec![0,3,6,9,12,15]));
+        assert_eq!(28, extrapolate_new_number(vec![1,3,6,10,15,21]));
+        assert_eq!(68, extrapolate_new_number(vec![10,13,16,21,30,45]));
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod day05;
 mod day06;
 mod day07;
 mod day08;
+mod day09;
 
 fn main() {
     //day01::day_01();
@@ -16,5 +17,6 @@ fn main() {
     //day05::day_05();
     //day06::day_06();
     //day07::day_07();
-    day08::day_08();
+    //day08::day_08();
+    day09::day_09();
 }
